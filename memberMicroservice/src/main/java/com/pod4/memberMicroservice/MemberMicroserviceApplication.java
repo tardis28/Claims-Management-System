0064ab77c7e972895df2d5c6ac95af1a329d69b1
tenne <tenne@DESKTOP-NJPD79G>
2021-07-08 22:37:29 +0530

    adding member microservice skeleton

---
+++ memberMicroservice/src/main/java/com/pod4/memberMicroservice/MemberMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pod4.memberMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberMicroserviceApplication.class, args);
+	}
+
+}
---
+++ memberMicroservice/src/test/java/com/pod4/memberMicroservice/MemberMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pod4.memberMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
