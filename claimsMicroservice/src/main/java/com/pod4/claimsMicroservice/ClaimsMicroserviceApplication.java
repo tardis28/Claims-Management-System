cd78df3c8a7cb17dbb3a81825c552f5b9b15ac4a
LENOVO <LENOVO@Droid>
2021-07-08 22:50:37 +0530

    adding claimsMicroservice skeleton

---
+++ claimsMicroservice/src/main/java/com/pod4/claimsMicroservice/ClaimsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pod4.claimsMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsMicroserviceApplication.class, args);
+	}
+
+}
