7919a05ec400d1077c27a4b0774220636e65a32e
Ayush Dwivedi <ayushdwivedi@192.168.0.102>
2021-07-08 23:31:29 +0530

    adding policy microservice skeleton

---
+++ policyMicroservice/src/test/java/com/pod4/policyMicroservice/PolicyMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pod4.policyMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
